76d1c3a1a03c14f56951d3776770a5866cdabd65
Liplan Lekipising <leeplanke@gmail.com>
2024-10-01 10:09:54 +0300

    add add patient button

--- src/app/(dashboard)/dashboard/patients/page.tsx
+++ src/app/(dashboard)/dashboard/patients/page.tsx
@@ -5,6 +5,24 @@ export default function DashboardOverview() {
   return (
     <section>
       <PatientTable />
+      <button className="h-[45px] w-[160px] absolute right-16 top-20 text-white bg-[#1F5AF4] text-sm flex justify-center items-center gap-2 font-semibold">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-plus"
+        >
+          <path d="M5 12h14" />
+          <path d="M12 5v14" />
+        </svg>
+        Add Patient
+      </button>
     </section>
   );
 }
