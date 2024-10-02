2e18cd5de5d86412a44112cef5a867efbef65b25
Liplan Lekipising <leeplanke@gmail.com>
2024-10-02 12:18:23 +0300

    deliveries side bar filters

---
+++ src/app/(dashboard)/dashboard/deliveries/components/deliveries.table.tsx
@@ -0,0 +1,162 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { patientsData } from "../../patients/components/data";
+
+function DeliveriesTable() {
+  return (
+    <div className="w-full flex-1 mt-8">
+      <div className="flex justify-between items-center mb-4">
+        <div className="flex items-center gap-2">
+          <label htmlFor="sort" className="mr-2 text-[#262626CC] text-sm">
+            Sort by
+          </label>
+          <button className="flex items-center gap-2 cursor-pointer">
+            <span className="text-[#262626] font-semibold">Hospital ID</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-chevron-down text-[#262626]"
+            >
+              <path d="m6 9 6 6 6-6" />
+            </svg>
+          </button>
+        </div>
+
+        <div className="relative">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-search absolute left-4 text-[#CFCFCF] top-1/2 -translate-y-1/2"
+          >
+            <circle cx="11" cy="11" r="8" />
+            <path d="m21 21-4.3-4.3" />
+          </svg>
+          <input
+            type="text"
+            placeholder="Search by patient name, id"
+            className="border border-[#CFCFCF] p-2 pl-12 focus:outline-none focus:border-[#276DF7] text-sm"
+          />
+        </div>
+      </div>
+
+      <div className="w-auto mx-8 flex flex-col">
+        <table className="min-w-full bg-white">
+          <thead>
+            <tr className="w-full border-b text-left text-sm font-semibold text-[#2A2A2AB2] tracking-wider">
+              <th className="p-2 py-6">Hospital ID</th>
+              <th className="p-2 py-6">Patient&apos;s Name</th>
+              <th className="p-2 py-6">Phone Number</th>
+              <th className="p-2 py-6">Next Delivery Date</th>
+              <th className="p-2 py-6">Location</th>
+              <th className="p-2 py-6">Status</th>
+              <th className="p-2 py-6">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {patientsData.map((patient, idx) => (
+              <PatientRow key={idx} patient={patient} />
+            ))}
+          </tbody>
+        </table>
+
+        <div className="w-full mt-8 flex justify-between">
+          <span className="text-[#26262699] text-sm">
+            You’re viewing 10 out of 87 deliveries
+          </span>
+
+          <div className="flex items-center gap-4">
+            <button className="border-[1px] border-[#26262633] h-[30px] w-[63px] text-sm text-[#26262699] rounded-2xl hover:bg-[#26262699] hover:text-white transition-all ease-in duration-150">
+              Prev
+            </button>
+            <button className="border-[1px] border-transparent h-[30px] w-[30px] text-sm text-[#26262699] rounded-2xl hover:bg-[#26262699] hover:text-white transition-all ease-in duration-150">
+              1
+            </button>
+            <button className="border-[1px] border-[#26262633] h-[30px] w-[30px] text-sm text-[#26262699] rounded-2xl hover:bg-[#26262699] hover:text-white transition-all ease-in duration-150">
+              2
+            </button>
+            <button className="border-[1px] border-transparent h-[30px] w-[30px] text-sm text-[#26262699] rounded-2xl hover:bg-[#26262699] hover:text-white transition-all ease-in duration-150">
+              3
+            </button>
+            <button className="border-[1px] border-transparent h-[30px] w-[30px] text-sm text-[#26262699] rounded-2xl hover:bg-[#26262699] hover:text-white transition-all ease-in duration-150">
+              4
+            </button>
+            <span>...</span>
+            <button className="border-[1px] border-transparent h-[30px] w-[30px] text-sm text-[#26262699] rounded-2xl hover:bg-[#26262699] hover:text-white transition-all ease-in duration-150">
+              8
+            </button>
+            <button className="border-[1px] border-[#26262633] h-[30px] w-[63px] text-sm text-[#26262699] rounded-2xl  hover:bg-[#26262699] hover:text-white transition-all ease-in duration-150">
+              Next
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DeliveriesTable;
+
+function PatientRow({
+  patient,
+}: {
+  patient: {
+    hospitalID: string;
+    name: string;
+    phone: string;
+    nextDeliveryDate: string;
+    location: string;
+    status: string;
+    id: string;
+  };
+}) {
+  const statusColor = () => {
+    switch (patient.status) {
+      case "Completed":
+        return "bg-[#01A85A33] text-[#01A85A]";
+      case "Due & Paid":
+        return "bg-[#FF7A0033] text-[#FF7A00]";
+      case "Due & Unpaid":
+        return "bg-[#F42C1F33] text-[#F42C1F]";
+      default:
+        return "bg-[#006AE733] text-[#006AE7]";
+    }
+  };
+
+  return (
+    <tr className="border-b text-sm text-[#2A2A2A]">
+      <td className="p-2 py-6">{patient.hospitalID}</td>
+      <td className="p-2 py-6">{patient.name}</td>
+      <td className="p-2 py-6">{patient.phone}</td>
+      <td className="p-2 py-6">{patient.nextDeliveryDate}</td>
+      <td className="p-2 py-6">{patient.location}</td>
+      <td className="p-2 py-6">
+        <span className={`inline-block px-5 py-2 font-bold ${statusColor()}`}>
+          {patient.status}
+        </span>
+      </td>
+      <td className="p-2 py-6">
+        <Link
+          href={`/dashboard/patients/${patient.id}/view`}
+          className="text-[#276DF7] px-6 hover:bg-[#276DF7] hover:text-white transition-all ease-in duration-150 py-2 font-bold hover:underline border-[1px] border-[#276DF799]"
+        >
+          View
+        </Link>
+      </td>
+    </tr>
+  );
+}
--- src/app/(dashboard)/dashboard/deliveries/page.tsx
+++ src/app/(dashboard)/dashboard/deliveries/page.tsx
@@ -1,5 +1,104 @@
 import React from "react";
+import DeliveriesTable from "./components/deliveries.table";
+import Link from "next/link";
 
-export default function Deliveries() {
-  return <section className="p-8">Coming Soon</section>;
+export default function Deliveries({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+  const deliveriesFilters = {
+    unassigned: [
+      {
+        title: "Paid",
+        count: 10,
+        query: "paid",
+      },
+      {
+        title: "Unpaid",
+        count: 12,
+        query: "unpaid",
+      },
+    ],
+    assigned: [
+      {
+        title: "Pending",
+        count: 10,
+        query: "pending",
+      },
+      {
+        title: "Successful",
+        count: 5,
+        query: "successful",
+      },
+      {
+        title: "Failed",
+        count: 3,
+        query: "failed",
+      },
+    ],
+  };
+  return (
+    <section className="p-8 flex gap-32">
+      <div className="w-[300px] flex flex-col gap-16">
+        <div className="flex flex-col gap-4">
+          <span className="font-bold text-[#2A2A2AB2]">
+            Unassigned Deliveries
+          </span>
+          {deliveriesFilters.unassigned.map((filter) => (
+            <OneSideItem
+              key={filter.title}
+              text={filter.title}
+              to={`/dashboard/deliveries?filter=${filter.query}`}
+              isActive={searchParams?.filter === filter.query}
+              count={filter.count}
+            />
+          ))}
+        </div>
+        <div className="flex flex-col gap-4">
+          <span className="font-bold text-[#2A2A2AB2]">
+            Assigned Deliveries
+          </span>
+          {deliveriesFilters.assigned.map((filter) => (
+            <OneSideItem
+              key={filter.title}
+              text={filter.title}
+              to={`/dashboard/deliveries?filter=${filter.query}`}
+              isActive={searchParams?.filter === filter.query}
+              count={filter.count}
+            />
+          ))}
+        </div>
+      </div>
+      <DeliveriesTable />
+    </section>
+  );
+}
+
+function OneSideItem({
+  text,
+  to,
+  isActive,
+  count,
+}: {
+  text: string;
+  to: string;
+  isActive: boolean;
+  count: number;
+}) {
+  return (
+    <Link
+      href={to}
+      className={`w-full flex justify-start pl-8 relative h-[60px] items-center border-r-[3px] ${
+        isActive
+          ? "font-bold text-[#276DF7] bg-[#276DF71A] text-base border-[#276DF7]"
+          : "font-medium text-sm text-[#2A2A2A80] bg-transparent border-transparent"
+      } hover:bg-[#276DF71A] hover:text-[#276DF7] transition-all ease-in duration-150`}
+    >
+      {text}
+      <span className="absolute right-8 flex justify-center items-center text-white font-medium text-sm bg-[#F42C1F] h-[25px] w-[25px] rounded-full">
+        {count}
+      </span>
+    </Link>
+  );
 }
