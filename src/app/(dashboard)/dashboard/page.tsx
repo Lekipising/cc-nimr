b8a6b0e420f6f4c2f1771fe16d47f93307bb7cf5
Liplan Lekipising <leeplanke@gmail.com>
2024-10-02 14:49:46 +0300

    uniform nav behaviour

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Overview() {
-  return <section className="p-8">Coming Soon</section>;
+  return <section className="p-8 min-h-screen">Coming Soon</section>;
 }
