840412c4661416fd60c07103238496d4a9d01541
Liplan Lekipising <leeplanke@gmail.com>
2024-10-02 10:39:29 +0300

    scan package logic

--- src/app/(dashboard)/dashboard/patients/[patientID]/assign-package/scan-package/page.tsx
+++ src/app/(dashboard)/dashboard/patients/[patientID]/assign-package/scan-package/page.tsx
@@ -9,6 +9,12 @@ export default function ScanPackage() {
 
   const qrScanner = useRef<QrScanner>();
 
+  const [scanned, setScanned] = useState(false);
+
+  const [code, setCode] = useState("");
+
+  const [scanStepCompleted, setScanStepCompleted] = useState(false);
+
   useEffect(() => {
     const videoElem = document.getElementById("scan-video") as HTMLVideoElement;
 
@@ -26,6 +32,11 @@ export default function ScanPackage() {
       // close after 5 seconds
       setTimeout(() => {
         qrScanner?.current?.stop();
+        setScanned(true);
+        setTimeout(() => {
+          setCode("1234567890");
+          setScanStepCompleted(true);
+        }, 2500);
       }, 5000);
     }
   }, [chosenOption]);
@@ -70,11 +81,20 @@ export default function ScanPackage() {
             <input
               type="text"
               placeholder="Enter Code"
+              value={code}
+              onChange={(e) => setCode(e.target.value)}
               className="border-[1px] border-[#E0E0E0] h-[60px] p-4 w-[245px] focus:border-[#1F5AF4] focus-within:border-[#1F5AF4]"
             />
 
             <button
-              onClick={() => setChosenOption("code")}
+              onClick={() => {
+                if (code.length === 0) {
+                  alert("Please enter a code");
+                  return;
+                }
+                setChosenOption("manual");
+                setScanStepCompleted(true);
+              }}
               className="h-[60px] mt-auto w-[245px] border-[#1F5AF4] text-[#276DF7] border-[1px] text-sm flex justify-center items-center gap-2 font-semibold"
             >
               Submit Code
@@ -83,17 +103,72 @@ export default function ScanPackage() {
         </div>
       ) : (
         <div className="flex flex-col gap-8 items-center mx-auto">
-          {chosenOption === "scan" ? (
+          {chosenOption === "scan" && (
             <div className="w-full">
-              <video className="h-[300px] w-[300px]" id="scan-video"></video>
+              {scanned ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="lucide lucide-circle-check h-32 w-32 text-[#01A85A]"
+                >
+                  <circle cx="12" cy="12" r="10" />
+                  <path d="m9 12 2 2 4-4" />
+                </svg>
+              ) : (
+                <video className="h-[300px] w-[300px]" id="scan-video"></video>
+              )}
 
-              <p className="text-[#0D0E1E] font-semibold mt-8 text-center w-full">
-                Scanning Package...
-              </p>
+              {!scanned ? (
+                <p className="text-[#0D0E1E] font-semibold mt-8 text-center w-full">
+                  Scanning Package...
+                </p>
+              ) : (
+                <p className="text-[#0D0E1E] font-semibold mt-8 text-center w-full">
+                  Package successfully scanned
+                </p>
+              )}
             </div>
-          ) : (
-            <div>
-              <Image src="/code.png" width={300} height={300} alt="Scan" />
+          )}
+
+          {scanStepCompleted && (
+            <div className="w-full flex flex-col justify-center">
+              <span className="text-[#2A2A2A]">Package Code</span>
+              {/* rectangle with code inside */}
+              <div className="border-[1px] border-[#E0E0E0] relative text-[#5E6C84] font-bold h-[60px] mt-2 p-4 w-[245px] flex justify-center items-center bg-[#E0E0E0]">
+                <span className="text-[#2A2A2A]">{code}</span>
+                <button
+                  className="text-[#F42C1F] flex items-center absolute -bottom-8 right-0 text-sm font-semibold"
+                  onClick={() => {
+                    setCode("");
+                    setScanStepCompleted(false);
+                    setChosenOption("");
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="lucide lucide-x mr-1 w-4 h-4"
+                  >
+                    <path d="M18 6 6 18" />
+                    <path d="m6 6 12 12" />
+                  </svg>
+                  Remove
+                </button>
+              </div>
             </div>
           )}
         </div>
